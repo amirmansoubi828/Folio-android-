99766fb7fdbd5932f6230475ce81fc926a626737
amirmansoubi <amirmansoubi828@gmail.com>
2018-06-15 10:38:15 +0430

    adding news feature

--- app/src/main/java/a4/folio/Page/NewsPage.java
+++ app/src/main/java/a4/folio/Page/NewsPage.java
@@ -1,10 +1,12 @@
 package a4.folio.Page;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by amir on 6/2/2018.
  */
 
-public class NewsPage {
+public class NewsPage extends AppCompatActivity{
     //// TODO: 6/2/2018
     //also needs a .xml layout
 }
