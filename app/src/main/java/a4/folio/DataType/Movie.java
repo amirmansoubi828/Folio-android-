23963f9c28f00ca59bf363185946cac4664d549b
amirmansoubi <amirmansoubi828@gmail.com>
2018-07-08 15:06:38 +0430

    movie data type added

---
+++ app/src/main/java/a4/folio/DataType/Movie.java
@@ -0,0 +1,29 @@
+package a4.folio.DataType;
+
+/**
+ * Created by amir on 7/8/2018.
+ */
+
+public class Movie {
+    private String name , date , length , size , link ;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getLink() {
+        return link;
+    }
+}
